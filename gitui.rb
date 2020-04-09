9305e47ed4b8a3a8aea73162074be8be1968915c
Stephan Dilly <dilly.stephan@gmail.com>
2020-04-09 09:38:51 +0200

    new release

--- gitui.rb
+++ gitui.rb
@@ -1,9 +1,9 @@
 class Gitui < Formula
     desc "Blazing fast terminal-ui for git written in rust"
     homepage "https://github.com/extrawurst/gitui"
-    url "https://github.com/extrawurst/gitui/releases/download/rc2/gitui-mac.tar.gz"
-    sha256 "49e7853a77c70efbbe6ed243c73a36d7f3ca8ea784a1f0275814208107de4045"
-    version "rc2"
+    url "https://github.com/extrawurst/gitui/releases/download/rc3/gitui-mac.tar.gz"
+    sha256 "340f39c8112026a91520405b339227cc0a8298131b2a0f6907898fb4d0644cf0"
+    version "rc3"
   
     bottle :unneeded
   
