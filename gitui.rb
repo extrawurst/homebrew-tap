2a98c2fdfea94dcdac8b8fd38ad1a474c69af9e7
runner <runner@Mac-1656.local>
2020-04-15 19:40:35 +0000

    gitui v0.1.8

--- gitui.rb
+++ gitui.rb
@@ -1,9 +1,9 @@
 class Gitui < Formula
     desc "Blazing fast terminal-ui for git written in rust"
     homepage "https://github.com/extrawurst/gitui"
-    url "https://github.com/extrawurst/gitui/releases/download/v0.1.7/gitui-mac.tar.gz"
-    sha256 "8ef1d032325b517b1c4ef0928ccca019c8de0ffe7872e8116c527cd834e211af"
-    version "v0.1.7"
+    url "https://github.com/extrawurst/gitui/releases/download/v0.1.8/gitui-mac.tar.gz"
+    sha256 "b55162f418417eb6a36583c9ed62cbfedf8543d6300c69a373bdead14aee78ad"
+    version "v0.1.8"
   
     bottle :unneeded
   
